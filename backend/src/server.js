d56f7b244047fffb2321f4e9f95dba3a7dc75743
infordigi <renato.nascimento2009@gmail.com>
2020-04-03 22:51:37 -0300

    Implementação do websocket e finalização do mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb://root:mongodb@localhost:27017/week9?authSource=admin', {
   useUnifiedTopology: true,
@@ -14,10 +19,25 @@ mongoose.connect('mongodb://root:mongodb@localhost:27017/week9?authSource=admin'
   useCreateIndex: true
 });
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 //app.use(cors({ origin: 'http: 192.168.2.18 }))
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..' , 'uploads')));
 app.use(routes);
 
-app.listen(3001);
+server.listen(3001);
--- mobile/src/pages/List/index.js
+++ mobile/src/pages/List/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, SafeAreaView, Image, AsyncStorage } from "react-native";
+import socketio from "socket.io-client";
+import { ScrollView, SafeAreaView, Image, AsyncStorage, Alert } from "react-native";
 
 import SpotList from "../../components/SpotList";
 
@@ -9,6 +10,19 @@ import logo from "../../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('user')
+      .then(user_id => {
+        const socket = socketio('http://192.168.2.18:3001', {
+          query: { user_id },
+        })
+
+        socket.on('booking_response', booking => {
+          Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+        })
+      });
+  }, []);
 
   useEffect(() => {
     AsyncStorage.getItem('techs')
